5326c1cd3767148a26b4de4b3b0352f760d33e89
Rahul Singh <rsembedded@gmail.com>
2017-07-24 23:42:16 +0530

    my second commit

---
+++ Statistics.R
@@ -0,0 +1,31 @@
+# This file provid you basics statistcs
+# We work on mba data set that have three variables
+
+# libraries
+library(moments)
+
+
+mba <- read.csv("mba.csv")
+class(mba)
+sapply(mba,class)
+
+# Basic statistics
+summary(mba)
+mean(mba$gmat)
+# mode
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+Mode(mba$gmat)
+
+median(mba$gmat)
+sd(mba$gmat)
+var(mba$gmat)
+kurtosis(mba$gmat)
+skewness(mba$gmat)
+
+#
+
+
