4d4fe11e680fbdf0244b2b444dd4a7284233f58b
Rahul Singh <rsembedded@gmail.com>
2017-07-25 18:37:31 +0530

    third commit

--- Statistics.R
+++ Statistics.R
@@ -1,9 +1,12 @@
 # This file provid you basics statistcs
 # We work on mba data set that have three variables
 
+
 # libraries
 library(moments)
 
+#get working directory
+getwd()
 
 mba <- read.csv("mba.csv")
 class(mba)
@@ -33,3 +36,29 @@ mean(mba$gmat)
 sd(mba$gmat)
 pnorm(670, 711, 29)
 
+#check the data is normal form or not
+qqnorm(mba$gmat)
+qqline(mba$gmat)
+
+# data transformation
+# log > exponatial> Reciprocal > sqrt > 1/sqrt etc
+qqnorm(mba$workex)
+qqline(mba$workex)
+# hence work exprence is not normal distribution so we use transformation
+# we fist start to log transformation to go 1/sqrt transformation if data is not normal we use 
+# use not normal techniqs for analysis but drawback of not normal techniqes is that the have less analytics info
+# that's why we more try to normal distribution
+
+workexpt <- log(mba$workex)
+qqnorm(workexpt)
+qqline(workexpt)
+
+
+# Convert the whole data set in to z score
+
+full_z_score <- scale(mba)
+full_z_score
+
+# find out probability and z score vice versa
+qnorm(.975)   # probabilty of zscore
+pnorm(1.96)  #  z score to probability
